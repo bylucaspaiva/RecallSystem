4304d4da3af224696ac6ae26c2ee21af6a2980d1
Lucas Paiva <lucasp4iva@gmail.com>
2024-09-02 20:24:00 -0300

    feat: add recall DataContext

---
+++ RecallSystem.Infrastructure/Data/RecallDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecallSystem.Infrastructure.Data
+{
+    public class RecallDbContext :DbContext
+    {
+        public RecallDbContext(DbContextOptions<RecallDbContext> options) : base(options) { }
+    }
+}
