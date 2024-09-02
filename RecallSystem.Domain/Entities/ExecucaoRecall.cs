5ab507da85826375b18d0ae4f6eabe5b3a5b4253
Lucas Paiva <lucasp4iva@gmail.com>
2024-09-02 20:35:51 -0300

    feat: add entities

---
+++ RecallSystem.Domain/Entities/ExecucaoRecall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecallSystem.Domain.Entities
+{
+    public class ExecucaoRecall
+    {
+        public int Id { get; set; }
+        public int RecallId { get; set; }
+        public string Chassi { get; set; }
+        public DateTime? DataExecucao { get; set; }
+        public string Concessionaria { get; set; }
+        public Recall Recall { get; set; }
+    }
+}
---
+++ RecallSystem.Domain/Entities/Recall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecallSystem.Domain.Entities
+{
+    public class Recall
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataPublicacao { get; set; }
+    }
+}
